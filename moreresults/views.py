fb5809dba7935611ac90708baaa435f6f28dbcb2
bjornsen <bernsen@gmail.com>
2013-09-14 14:08:38 -0400

    Testing autogit

--- moreresults/views.py
+++ moreresults/views.py
@@ -18,7 +18,7 @@ log = logging.getLogger(__name__)
 def my_route_view(request):
     one = request.matchdict['one']
     two = request.matchdict['two']
-    return Response(one + ' ' + two)
+    return Response('GIT UPDATE IS WORKING\n' + one + ' ' + two)
 
 @view_config(route_name='git_autoupdate')
 def git_update(request):
